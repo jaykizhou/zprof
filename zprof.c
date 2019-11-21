a28b0c2dc4cc44307bd1a6133d674d2e2f99d554
jaykizhou <yiqianniyiye@126.com>
2019-11-21 22:42:28 +0800

    函数执行后收集sql

--- zprof.c
+++ zprof.c
@@ -1871,13 +1871,13 @@ void hp_mode_hier_beginfn_cb(hp_entry_t **entries, hp_entry_t *current, zend_exe
     zp_trace_callback *callback;
     int recurse_level = 0;
 
-    if (data != NULL)
-    {
-        if (hp_trace_callbacks_filter_exist(current->hash_code TSRMLS_CC) && zend_hash_find(ZP_G(trace_callbacks), current->name_hprof, strlen(current->name_hprof) + 1, (void **)&callback) == SUCCESS)
-        {
-            (*callback)(current->name_hprof, data TSRMLS_CC);
-        }
-    }
+    // if (data != NULL)
+    // {
+    //     if (hp_trace_callbacks_filter_exist(current->hash_code TSRMLS_CC) && zend_hash_find(ZP_G(trace_callbacks), current->name_hprof, strlen(current->name_hprof) + 1, (void **)&callback) == SUCCESS)
+    //     {
+    //         (*callback)(current->name_hprof, data TSRMLS_CC);
+    //     }
+    // }
 
     if ((ZP_G(zprof_flags) & ZPROF_FLAGS_NO_HIERACHICAL) == 0)
     {
@@ -1959,6 +1959,15 @@ void hp_mode_hier_endfn_cb(hp_entry_t **entries, zend_execute_data *data TSRMLS_
     /* Get end tsc counter */
     tsc_end = cycle_timer();
     wt = get_us_from_tsc(tsc_end - top->tsc_start TSRMLS_CC);
+
+    zp_trace_callback *callback;
+    if (data != NULL)
+    {
+        if (hp_trace_callbacks_filter_exist(top->hash_code TSRMLS_CC) && zend_hash_find(ZP_G(trace_callbacks), top->name_hprof, strlen(top->name_hprof) + 1, (void **)&callback) == SUCCESS)
+        {
+            (*callback)(top->name_hprof, data TSRMLS_CC);
+        }
+    }
 
     // 可以考虑只记录执行时间大于 0.1 ms的函数
     //if (wt >= ZP_G(stack_threshold) * 1000)
